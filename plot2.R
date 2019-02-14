137a742059d074449e02c4b39dd1988fbbe54f0a
David Searl <dhsearl@gmail.com>
2019-02-14 08:55:20 -0600

    Cleaned up and added Comments

--- plot2.R
+++ plot2.R
@@ -9,13 +9,16 @@ if (!file.exists(zipFile)) {
   download.file(zip_url, zipFile, mode = "wb")
 }
 
+# Get just the rows we need. Add column names.
 datas <- read_delim(zipFile, delim = ";", na = "?", skip=66637, n_max=2880, col_names=F)
 colnames(datas) <- read_delim(zipFile, n_max = 1, col_names=F,delim=";")
+
+# Convert Dates and create Datetime column.
 datas$Date <- as.Date(datas$Date, "%d/%m/%Y")
 datas$Datetime <- strptime(paste(datas$Date, datas$Time), "%Y-%m-%d %H:%M:%S")
 
 
-#plot2
+# plot2
 png(filename = "plot2.png", width=480, height=480)
 plot(datas$Datetime, datas$Global_active_power, type="l", ylab = "Global Active Power (kilowatts)",xlab="")
 dev.off()
