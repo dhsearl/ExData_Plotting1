c7a6f78b959e478837d75c94d710ad120db88412
David Searl <dhsearl@gmail.com>
2019-02-14 08:51:37 -0600

    Cleaned up, Added Comments

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 library(readr)
-#library(lubridate)
+
 
 # download zip file containing data if it hasn't already been downloaded
 zip_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
@@ -9,8 +9,11 @@ if (!file.exists(zipFile)) {
   download.file(zip_url, zipFile, mode = "wb")
 }
 
+# Get just the rows we need. Add column names.
 datas <- read_delim(zipFile, delim = ";", na = "?", skip=66637, n_max=2880, col_names=F)
 colnames(datas) <- read_delim(zipFile, n_max = 1, col_names=F,delim=";")
+
+# Convert Dates and create Datetime column.
 datas$Date <- as.Date(datas$Date, "%d/%m/%Y")
 datas$Datetime <- strptime(paste(datas$Date, datas$Time), "%Y-%m-%d %H:%M:%S")
 
